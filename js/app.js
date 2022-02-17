f58363c894290779d6fffe295fea0e203e3d19d9
sarafat27 <sarafat2769@gmail.com>
2022-02-17 13:23:52 +0600

    saving amount completed

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//getting value of input field
 function getInputValue(type) {
     const fieldName = document.getElementById(type + '-input');
     const inputValue = parseFloat(fieldName.value);
@@ -5,19 +6,38 @@ function getInputValue(type) {
 
 }
 
+//getting result field
 function getResultField(elementId) {
     const resultField = document.getElementById(elementId);
     return resultField;
 }
 
 
-
 //click handler for calculate button
 document.getElementById('calculate-btn').addEventListener('click', function () {
-    const incomeValue = getInputValue('income');
+    //showing total expenses
     const foodValue = getInputValue('food');
     const rentValue = getInputValue('rent');
     const clothesValue = getInputValue('clothes');
     const totalExpenses = foodValue + rentValue + clothesValue;
     getResultField('total-expenses').innerText = totalExpenses;
+    //showing balance
+    const incomeValue = getInputValue('income');
+    const balance = incomeValue - totalExpenses;
+    getResultField('total-balance').innerText = balance;
+
+});
+
+//click handler for save button
+document.getElementById('save-button').addEventListener('click', function () {
+    //showing saving amount
+    const savepercent = getInputValue('save');
+    const incomeValue = getInputValue('income');
+    const savingAmount = incomeValue * (savepercent / 100);
+    getResultField('saving-amount').innerText = savingAmount;
+    //showing remaining balance
+    const balance = getResultField('total-balance').innerText;
+    const remainingBalance = balance - savingAmount;
+    getResultField('remaining-balance').innerText = remainingBalance;
 })
+
